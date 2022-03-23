7da29c8bf89454d07173c630e1154bb021627ab7
leafhub <1803020+hrs-nkym@users.noreply.github.com>
2022-03-23 21:23:17 +0900

    first init

---
+++ lib/auth_service.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+// 1
+enum AuthFlowStatus { login, signUp, verification, session }
+
+// 2
+class AuthState {
+  final AuthFlowStatus authFlowStatus;
+
+  AuthState({required this.authFlowStatus});
+}
+
+// 3
+class AuthService {
+  // 4
+  final authStateController = StreamController<AuthState>();
+
+  // 5
+  void showSignUp() {
+    final state = AuthState(authFlowStatus: AuthFlowStatus.signUp);
+    authStateController.add(state);
+  }
+
+  // 6
+  void showLogin() {
+    final state = AuthState(authFlowStatus: AuthFlowStatus.login);
+    authStateController.add(state);
+  }
+
+  // 7
+  // 1
+  // void loginWithCredentials(AuthCredentials credentials) {
+  //   final state = AuthState(authFlowStatus: AuthFlowStatus.session);
+  //   authStateController.add(state);
+  // }
+
+// 2
+  // void signUpWithCredentials(SignUpCredentials credentials) {
+  //   final state = AuthState(authFlowStatus: AuthFlowStatus.verification);
+  //   authStateController.add(state);
+  // }
+}
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/sign_up_page.dart';
+
+import 'auth_service.dart';
+import 'login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _authService = AuthService();
+
+  @override
+  void initState() {
+    super.initState();
+    _authService.showLogin();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Photo Gallery App',
+      theme: ThemeData(visualDensity: VisualDensity.adaptivePlatformDensity),
+      // 2
+      home: StreamBuilder<AuthState>(
+          // 2
+          stream: _authService.authStateController.stream,
+          builder: (context, snapshot) {
+            // 3
+            if (snapshot.hasData) {
+              return Navigator(
+                pages: [
+                  // 4
+                  // Show Login Page
+                  if (snapshot.data?.authFlowStatus == AuthFlowStatus.login)
+                    MaterialPage(
+                        child: LoginPage(
+                            shouldShowSignUp: _authService.showSignUp)),
+
+                  // 5
+                  // Show Sign Up Page
+                  if (snapshot.data?.authFlowStatus == AuthFlowStatus.signUp)
+                    MaterialPage(
+                        child:
+                            SignUpPage(shouldShowLogin: _authService.showLogin))
+                ],
+                onPopPage: (route, result) => route.didPop(result),
+              );
+            } else {
+              // 6
+              return Container(
+                alignment: Alignment.center,
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
+    );
+  }
+}
